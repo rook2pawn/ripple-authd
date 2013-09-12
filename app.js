66aec971db226f10b57f17a3405cd818680f78fd
Stefan Thomas <justmoon@members.fsf.org>
2013-09-11 22:57:48 -0700

    Enable SSL for production deployment.

--- app.js
+++ app.js
@@ -5,26 +5,39 @@
 
 var express = require('express')
   , api = require('./routes/api')
+  , fs = require('fs')
+  , path = require('path')
   , http = require('http')
+  , https = require('https')
   , path = require('path')
   , config = require('./config');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', config.port || 3000);
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
 });
 
+var server;
 app.configure('development', function(){
+  app.set('port', config.port || 3000);
+  server = http.createServer(app);
   app.use(express.errorHandler());
+});
+
+app.configure('production', function(){
+  app.set('port', config.port || 443);
+  server = https.createServer({
+    key: fs.readFileSync(path.resolve(__dirname, 'ssl.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'ssl.crt'))
+  }, app);
 });
 
 app.post('/api/sign', api.sign);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
