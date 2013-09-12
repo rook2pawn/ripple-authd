c35f71a543897692b8e2d5fbe90e6ef5ab26f5fb
Stefan Thomas <justmoon@members.fsf.org>
2013-09-12 08:10:05 -0700

    Add some error handling.

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,27 @@ var Signer = require('../lib/signer').Signer;
 
 var signer = new Signer();
 
+function returnError(res, type, message) {
+  var data = {
+    result: 'error',
+    error: type,
+    error_message: message
+  };
+  res.send(JSON.stringify(data));
+}
+
 exports.sign = function(req, res, next){
   var signres;
   try {
+    if ("string" !== typeof req.body.info) {
+      returnError(res, "missingInfo", "Public information is missing.");
+    }
+    if ("string" !== typeof req.body.signreq) {
+      returnError(res, "missingSignreq", "Signature request is missing.");
+    }
     signres = signer.sign(""+req.body.info, ""+req.body.signreq);
   } catch (e) {
-    next(e);
+    returnError(res, "internalError", e.message);
     return;
   }
   var data = {
