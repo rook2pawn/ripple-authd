030148d7fdb64bfc63b290d0374e2ef46804db06
Stefan Thomas <justmoon@members.fsf.org>
2013-09-12 07:52:39 -0700

    Add CA certificate.

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.set('port', config.port || 443);
   server = https.createServer({
+    ca: fs.readFileSync(path.resolve(__dirname, 'ca.crt')),
     key: fs.readFileSync(path.resolve(__dirname, 'ssl.key')),
     cert: fs.readFileSync(path.resolve(__dirname, 'ssl.crt'))
   }, app);
